f5a8f4da013a7cbe212f039f5052ff9728d368d0
Vinicius Santos <165097853+RosiestSloth@users.noreply.github.com>
2025-11-25 21:13:06 -0300

    Update Pizzaria.py
    Adição de funcionalidades da simulação de crud

--- Lógica/Ex-02/Pizzaria.py
+++ Lógica/Ex-02/Pizzaria.py
@@ -34,6 +34,26 @@ def opcoes(tipo):
         id_funcionario = int(input("Digite o [id] do Funcionário: "))
         
         editarFuncionario(id_funcionario)
+    elif tipo == "menuCliente":
+        print("-=" * 20)
+        print(f"{'EDITAR CLIENTE':^40}")
+        print("-=" * 20)
+
+        listarClientes()
+
+        id_cliente = int(input("Digite o [id] do Funcionário: "))
+        
+        editarCliente(id_cliente)
+    
+    elif tipo == "menuCliente":
+        print("-=" * 20)
+        print(f"{'REALIZAR PEDIDO':^40}")
+        print("-=" * 20)
+
+        listarClientes()
+        listarPizzas()
+        
+        realizarPedido()
 
 # Seção de Clientes
 def listarClientes():
@@ -67,6 +87,17 @@ def CadastroClientes():
         print("Erro ao Cadastrar Cliente: ", e)
         sleep(0.5)
         CadastroClientes()
+
+def editarCliente(id_cliente):
+    try:
+        print(f"Editando informações de {funcionarios[id_cliente]["nome"]}")
+        
+        clientes[id_cliente]["nome"] = str(input("Digite o nome do Funcionário: "))
+        clientes[id_cliente]["contato"] = str(input("Digite o contato do Funcionário: "))
+        clientes[id_cliente]["endereco"] = str(input("Digite o tipo de Funcionário: "))
+
+    except Exception as e:
+        print(f"Erro: valores inválidos: {e}")
 
 def editarCliente(id_cliente):
     try:
@@ -113,6 +144,7 @@ def CadastroFuncionarios():
         CadastroFuncionarios()
 
 def editarFuncionario(id_funcionario):
+    """ Editar dados de funcionários da empresa """
     try:
         print(f"Editando informações de {funcionarios[id_funcionario]["nome"]}")
         
@@ -123,17 +155,59 @@ def editarFuncionario(id_funcionario):
     except Exception as e:
         print(f"Erro: valores inválidos: {e}")
 
+# Seção de Pizzas
+
+def cadastroPizzas():
+    """Função para a realização do cadastro de Pizzas do sistema"""
+    global qtd_pizzas
+    try:
+        pizza = {}
+        pizza["nome"] = str(input("Digite o nome da Pizza: "))
+        pizza["engredientes"] = str(input("Digite os engredientes: "))
+        pizza["valor"] = float(input("Digite o valor: "))
+        pizzas.append(pizza)
+
+        print("Pizza cadastrada com Sucesso!")
+        qtd_pizzas += 1
+        sleep(1)
+
+    except Exception as e:
+        print("Erro ao cadastrar Pizza", e)
+        sleep(0.5)
+        cadastroPizzas()
+
+def listarPizzas():
+    print("Lista de Pizzas")
+
+    print(f"{'[ID]':<6} {'Nome':<15} {'engredientes':<25} {'valor'}")
+
+    for c in range(qtd_pizzas):
+        print(f"[{c:^2}]   {pizzas[c]['nome']:<25} {pizzas[c]['engredientes']:<15} {pizzas[c]['valor']}")
+        sleep(0.2)
+
+    print("-=" * 20)
+    sleep(1)
+
+# Seção de Pedidos
+
+def realizarPedido():
+    id_cliente = int(input(("Digite o [id] do Cliente: ")))
+    id_pizza = int(input("Digite o [id] da Pizza: "))
 
 
-cliente = {"nome": "Matheus", "contato": "99 9999-9999", "endereco": "Rua 2 Lote 10 Casa Nº 950"}
-funcionario = {"nome": "Adélio", "contato": "99 9999-9999", "role": "Administrador"}
+cliente = { "nome": "Matheus", "contato": "99 9999-9999", "endereco": "Rua 2 Lote 10 Casa Nº 950" }
+funcionario = { "nome": "Adélio", "contato": "99 9999-9999", "role": "Administrador" }
+pizza = { "nome": "4 Queijos", "engredientes": "Mussarela, parmesão, sheddar", "valor": 20.00 }
 clientes = []
 funcionarios = []
+pizzas = []
 option = ""
 clientes.append(cliente)
 funcionarios.append(funcionario)
+pizzas.append(pizza)
 qtd_clientes = 1
 qtd_funcionarios = 1
+qtd_pizzas = 1
 
 while option != 0:
     option = opcoes("menuInicio")
@@ -156,7 +230,7 @@ while option != 0:
         case 4:
             # Cadastrar Pizzas
             print("Cadastro de Pizzas")
-
+            cadastroPizzas()
         
         case 5:
             # Listagem de Clientes
@@ -178,6 +252,9 @@ while option != 0:
             print("Edição de Dados")
             opcoes("menuFuncionario")
 
+        case 9:
+            print("Edição de Dados")
+            opcoes("menuCliente")
         case 0:
             # Sair do loop
             print("Saindo...")
